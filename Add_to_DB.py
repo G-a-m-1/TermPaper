feea8d1bb470654ac69984e7545e1b5bc61d5112
Gam1 <vadimchaikovskij@gmail.com>
2026-01-05 22:28:13 +0200

    Добавьте файлы проекта.

---
+++ Add_to_DB.py
@@ -0,0 +1,93 @@
+﻿import os
+import re
+import Ollama_manager
+from concurrent.futures import ThreadPoolExecutor
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaEmbeddings
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+# Налаштування
+DB_DIR = "./db"
+DOCS_DIR = os.path.join("Data", "D_pdfs")
+EMBEDDINGS = OllamaEmbeddings(model="qwen3-embedding:0.6b", num_ctx = 2048) #"nomic-embed-text"
+MAX_WORKERS = 12
+CHUNK_SIZE = 2000
+CHUNK_OVERLAP = 100
+TEXT_SPLITTER = RecursiveCharacterTextSplitter(
+    chunk_size=CHUNK_SIZE, 
+    chunk_overlap=CHUNK_OVERLAP,
+    separators=["\n\n", "\n", ".", "!", "?", " "] # Пріоритет розрізу для збереження сенсу
+)
+BATCH_SIZE = 10
+
+def process_single_pdf(file_path):
+    """Функція для обробки одного файлу"""
+    try:
+        loader = PyMuPDFLoader(file_path)
+        docs = loader.load()
+        return TEXT_SPLITTER.split_documents(docs)
+    except Exception as e:
+        print(f"Помилка при читанні {file_path}: {e}")
+        return []
+
+def clean_chunks(chunks):
+    cleaned = []
+    for d in chunks:
+        # Видаляю все, що не є текстом, цифрами, пунктуацією чи кирилицею
+        text = re.sub(r'[^\x20-\x7E\u0400-\u04FF\s]', '', text) 
+        # Нормалізація пробілів та переносів (текст одним рядком)
+        text = " ".join(text.split())
+        # Перевірка на мінімальну довжину
+        if len(text.strip()) < 30:
+            continue            
+        d.page_content = text
+        cleaned.append(d)
+    return cleaned
+
+def update_database():
+    """Функція для оновлення бази даних"""
+    vectorstore = Chroma(persist_directory=DB_DIR, embedding_function=EMBEDDINGS)
+    
+    # Отримання метаданих
+    existing_docs = vectorstore.get(include=['metadatas'])
+    existing_sources = {m['source'] for m in existing_docs['metadatas']} if existing_docs['metadatas'] else set()
+
+    files_to_process = [
+        os.path.join(DOCS_DIR, f) for f in os.listdir(DOCS_DIR) 
+        if f.lower().endswith(".pdf") and os.path.join(DOCS_DIR, f) not in existing_sources
+    ]
+
+    if not files_to_process:
+        print("Нових документів не знайдено.")
+        return
+
+    print(f"Знайдено {len(files_to_process)} нових файлів. Обробка...")
+
+    # Паралельне завантаження та розбиття на чанки
+    all_raw_chunks = []
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        results = list(executor.map(process_single_pdf, files_to_process))
+        for chunks in results:
+            all_raw_chunks.extend(chunks)
+
+    all_new_chunks = clean_chunks(all_raw_chunks)
+    # Додавання в базу
+    print(f"Створено {len(all_new_chunks)} фрагментів.")
+    for i in range(0, len(all_new_chunks), BATCH_SIZE):
+        print(f"Генерація ембеддінгів від {i} до {i + BATCH_SIZE} фрагменту...\t\t",end="")
+        batch = all_new_chunks[i : i + BATCH_SIZE]
+        try:
+            vectorstore.add_documents(batch)
+            print("Успішно!")
+        except Exception as e:
+            print(f"Пропущено фрагменти через помилку: {e}")
+
+if __name__ == "__main__":
+    Ollama_manager.start_ollama()
+    if not os.path.exists(DOCS_DIR):
+        os.makedirs(DOCS_DIR)
+    update_database()
+    Ollama_manager.stop_ollama()
+    print("Завершено.")
+    input()
---
+++ Ollama_manager.py
@@ -0,0 +1,53 @@
+﻿import subprocess
+import time
+import httpx  
+import os
+
+def start_ollama():
+    """Перевіряє, чи працює Ollama, і запускає її, якщо ні."""
+    url = "http://localhost:11434"
+    
+    try:
+        # Перевіряю, чи сервер уже працює
+        with httpx.Client(timeout=2) as client:
+            client.get(url)
+        print("Ollama вже запущена.")
+    except (httpx.ConnectError, httpx.HTTPError):
+        print("Запуск Ollama...")
+        subprocess.Popen(
+            ["ollama", "serve"],
+            creationflags=subprocess.CREATE_NEW_CONSOLE,
+        )
+        
+        # Чекаю, поки сервер почне відповідати (макс 30 секунд)
+        for i in range(30):
+            try:
+                with httpx.Client() as client:
+                    client.get(url)
+                print("Сервер Ollama готовий до роботи!")
+                return
+            except:
+                time.sleep(1)
+        print("Не вдалося запустити Ollama.")
+
+
+def stop_ollama():
+    """Завершує роботу всіх процесів Ollama."""
+    print("Вимкнення Ollama...")
+    try:
+        if os.name == 'nt':  # Для Windows
+            # /F — примусово, /IM — за іменем образу
+            subprocess.run(
+                ["taskkill", "/F", "/IM", "ollama.exe"], 
+                stdout=subprocess.DEVNULL, 
+                stderr=subprocess.DEVNULL
+            )
+        else:  # Для Linux / Mac
+            subprocess.run(
+                ["pkill", "ollama"], 
+                stdout=subprocess.DEVNULL, 
+                stderr=subprocess.DEVNULL)
+        
+        print("Ollama успішно вимкнена.")
+    except Exception as e:
+        print(f"Не вдалося вимкнути Ollama: {e}")
